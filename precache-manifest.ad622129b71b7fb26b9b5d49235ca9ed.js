bb132f0a166f30cf82266b1f4c23189287da1ff1
Oleksii Bespalko <ol.bespalko@gmail.com>
2019-02-17 21:46:30 +0200

    Updates

---
+++ precache-manifest.ad622129b71b7fb26b9b5d49235ca9ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dbf9f8e1176e1bc207c86fa6379ab14",
+    "url": "/face-detector/static/media/SourceCodePro-Regular.4dbf9f8e.ttf"
+  },
+  {
+    "revision": "7bf2d49a8f6160aeb9b0316458792aad",
+    "url": "/face-detector/static/media/SourceCodePro-ExtraLight.7bf2d49a.ttf"
+  },
+  {
+    "revision": "86b8a4d1ace465dddb7686f658fd25db",
+    "url": "/face-detector/static/media/SourceCodePro-Bold.86b8a4d1.ttf"
+  },
+  {
+    "revision": "81c0aae9bb2eb54bf8fe07e1b68d829e",
+    "url": "/face-detector/static/media/PressStart2P-Regular.81c0aae9.ttf"
+  },
+  {
+    "revision": "bd204893624f3fda3483",
+    "url": "/face-detector/static/js/runtime~main.bd204893.js"
+  },
+  {
+    "revision": "c590cbdf0f4a4dcd0208",
+    "url": "/face-detector/static/js/main.c590cbdf.chunk.js"
+  },
+  {
+    "revision": "5cb31957840a7bd58f00",
+    "url": "/face-detector/static/js/1.5cb31957.chunk.js"
+  },
+  {
+    "revision": "c590cbdf0f4a4dcd0208",
+    "url": "/face-detector/static/css/main.a90ce6a1.chunk.css"
+  },
+  {
+    "revision": "165e547aa7e65612294308f3d53050ae",
+    "url": "/face-detector/index.html"
+  }
+];
